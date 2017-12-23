3986b33ca11a4f02d2306ec0093ee381b4a69061
winterMute84 <wcarbajal84@gmail.com>
2017-12-23 14:25:35 -0800

    added footer and chaged layout on aboutPage

--- js/indexPage.js
+++ js/indexPage.js
@@ -2,6 +2,6 @@ function strobeAnimate(){
     document.querySelector(".notAnimated").className = "animated strobe";
 
     setTimeout(function () {
-      window.location.href = "aboutPage.html"; //will redirect to your main page
-   }, 7000); //will call the function after 2 secs.
+      window.location.href = "aboutPage.html"; //will redirect to your about page
+   }, 7000); //will call the function after 7 secs.
   }
