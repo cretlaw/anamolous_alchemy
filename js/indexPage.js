8854a9b837100581bd80826392bb4e70013b8269
winterMute84 <wcarbajal84@gmail.com>
2017-12-22 17:02:34 -0800

    changed file names and changed spacing and backround gradient in about page

--- js/indexPage.js
+++ js/indexPage.js
@@ -2,6 +2,6 @@ function strobeAnimate(){
     document.querySelector(".notAnimated").className = "animated strobe";
 
     setTimeout(function () {
-      window.location.href = "mainPage.html"; //will redirect to your main page
+      window.location.href = "aboutPage.html"; //will redirect to your main page
    }, 7000); //will call the function after 2 secs.
   }
